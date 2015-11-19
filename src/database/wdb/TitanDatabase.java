d2b7abf9dfcc4629d89fe60fcabd005f2206938e
Parker Timmins <parkert@cs.utexas.edu>
2015-11-18 23:23:03 -0600

    some

--- src/database/wdb/TitanDatabase.java
+++ src/database/wdb/TitanDatabase.java
@@ -1,25 +1,24 @@
-/*
- * Created on Feb 2, 2006
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
- */
+
 package wdb;
 
+import com.thinkaurelius.titan.core.PropertyKey;
 import com.thinkaurelius.titan.core.TitanFactory;
 import com.thinkaurelius.titan.core.TitanGraph;
 import com.thinkaurelius.titan.core.TitanTransaction;
+import com.thinkaurelius.titan.core.schema.TitanManagement;
+
+
 import wdb.metadata.ClassDef;
 import wdb.metadata.IndexDef;
 
-public class TitanDatabase implements DatabaseTool {    
+public class TitanDatabase implements DatabaseTool {
     private String databasePath;
     private TitanGraph graph;
-    
+
     public TitanDatabase(String databasePath) throws Exception
     {
-        this.databasePath = databasePath;   
-    } 
+        this.databasePath = databasePath;
+    }
 
     public void openSecDb(IndexDef index) throws Exception
     {
@@ -31,8 +30,30 @@ public class TitanDatabase implements DatabaseTool {
         this.graph = TitanFactory.build().
                 set("storage.backend", "berkeleyje").
                 set("storage.directory", databasePath).
-                open();
-    } 
+        open();
+        createSchema();
+    }
+    private void createSchema() throws Exception
+    {
+        TitanManagement mgmt = this.graph.openManagement();
+
+        final PropertyKey name = mgmt.makePropertyKey("name").dataType(String.class).make();
+        TitanManagement.IndexBuilder nameIndexBuilder = mgmt.buildIndex("name", Vertex.class).addKey(name);
+        if (uniqueNameCompositeIndex)
+            nameIndexBuilder.unique();
+
+        final PropertyKey time = mgmt.makePropertyKey("comment").dataType(String.class).make();
+        final PropertyKey time = mgmt.makePropertyKey("attributes").dataType(ArrayList<Attribute>.class).make();
+        final PropertyKey time = mgmt.makePropertyKey("instances").dataType(ArrayList<Integer>.class).make();
+        final PropertyKey time = mgmt.makePropertyKey("indexes").dataType(ArrayList<IndexDef>.class).make();
+        if (null != mixedIndexName)
+            mgmt.buildIndex("vertices", Vertex.class).addKey("comment").addKey("attributes").addKey("instances")
+                    .addKey("indexes").buildMixedIndex(mixedIndexName);
+
+        mgmt.makeVertexLabel("classDef").make();
+
+        mgmt.commit();
+    }
     public void closeDb() throws Exception
     {
         graph.close();
--- src/database/wdb/TitanDatabaseAdapter.java
+++ src/database/wdb/TitanDatabaseAdapter.java
@@ -1,37 +1,54 @@
 package wdb;
 
 import com.thinkaurelius.titan.core.TitanTransaction;
+import com.thinkaurelius.titan.core.schema.TitanGraphIndex;
+import wdb.metadata.Attribute;
 import wdb.metadata.ClassDef;
 import wdb.metadata.IndexDef;
 import wdb.metadata.WDBObject;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class TitanDatabaseAdapter implements DatabaseAdapter {
 
+    private TitanTransaction tx;
+    private TitanDatabase db;
 
     public TitanDatabaseAdapter(TitanDatabase database, TitanTransaction transaction) {
-
+        this.db = database;
+        this.tx = transaction;
     }
 
     @Override
     public void commit() throws Exception {
-
+        this.tx.commit();
     }
 
     @Override
     public void abort() throws Exception {
-
+        this.tx.rollback();
     }
 
     @Override
     public void putClass(ClassDef classDef) throws Exception {
+        Vertex x = tx.addVertex(T.label, "classDef", "name", classDef.name, "comment", classDef.comment, "attributes", classDef.attributes,
+                "instances", classDef.instances, "indexes", classDef.indexes);
 
     }
 
     @Override
     public ClassDef getClass(String className) throws Exception {
-        return null;
+        TitanGraphIndex index = mgmt.getGraphIndex(className);
+        String name = index.name();
+        String comment = index.getParametersFor("comment")[0];
+        return new ClassDef(name, comment){
+            this.name = name;
+            this.comment = comment;
+            this.attributes = new ArrayList<Attribute>(Arrays.asList(index.getParametersFor("attributes")));
+            this.instances = new ArrayList<Integer>(Arrays.asList(index.getParametersFor("instances")));
+            this.indexes = new ArrayList<IndexDef>(Arrays.asList(index.getParametersFor("indexes")));
+        };
     }
 
     @Override
