4bafb0a1c430888287c8780715184298d71b4d51
Parker Timmins <parkert@cs.utexas.edu>
2015-11-23 18:09:00 -0600

    made runner scripts

---
+++ makerun.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+./build.sh
+cd build/classes
+rm -rf thedb 
+export INSTANCE_ROOT="."
+java -Dlog4j.configuration="log4j.properties" -cp "../../lib/*:." wdb.WDB
+#java -Dlog4j.debug -Dlog4j.configuration="log4j.properties" -cp "../../lib/*:." wdb.WDB
+
+
